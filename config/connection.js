e85dd05e0783e7e5e220881055a92a036b07d7be
ichumats <ivanna.chumatschko22@gmail.com>
2020-02-08 14:25:35 -0700

    heroke fix attempt 2

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
 //Set up MySQL connection
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection(process.env.JAWSDB_URL || {
   host: 'localhost',
-  port: 3306,
   user: 'root',
   password: process.env.password,
   database: 'burgers_db'
--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 var express = require('express');
+// Set Handlebars
+var exphbs = require('express-handlebars');
+var db = require("./models");
+var mysql = require("mysql");
+
 var request = require('request')
 var app = express();
 require('dotenv').config();
 
 var PORT = process.env.PORT || 3000;
-var corsOptions = {
-  origin: process.env.ORIGIN_URL || "http://localhost",
-  optionsSuccessStatus: 200
-};
+
 
 // Serve static content for the app from the "public" directory in the application directory
 app.use(express.static('public'));
-
-app.use("/cors/*", function(req, res) {
-  req.pipe(request(req.params[0])).pipe(res);
-})
-
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Set Handlebars
-var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -31,8 +25,21 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use(routes);
 
+//error handler
+app.use(function(err,req,res,next){
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err: {};
+
+  //render the error page
+  res.status(err.status || 500);
+})
+
+
 //Start our server so that it can begin listening to client requests
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log('Server listening on http://locahost:' + PORT);
 });
+
+module.exports = app;
