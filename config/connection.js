e99c80b57419a1753af7c73a432f25fb969063ca
ichumats <ivanna.chumatschko22@gmail.com>
2020-02-20 11:12:15 -0700

    updates to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 //Set up MySQL connection
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection(JAWSDB_URL || {
   host: 'localhost',
   port: 3306,
   user: 'root',
